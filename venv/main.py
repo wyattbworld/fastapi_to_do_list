fc0a61696a4b8a2888d700b93457d098529f4642
wyattbworld <90436651+wyattbworld@users.noreply.github.com>
2025-11-02 15:12:38 -0700

    Trying to use uvicorn to listen on port 8080

--- venv/main.py
+++ venv/main.py
@@ -85,4 +85,4 @@ def uncomplete_todo(index: Annotated[int, Path(description="The index of the tod
     return todos[index]
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host='0.0.0.0')
+    uvicorn.run(app, port=8080, host='0.0.0.0')
